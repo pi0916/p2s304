54c8c358af205b65159d55edc0436ae2a68d23bb
PiMatrix <Pi.matrix@outlook.com>
2018-06-08 00:07:34 +1200

    all the query methods are done, need to do the transaction.

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -16,51 +16,49 @@ public class LibraryModel {
     //Initialization
     private final String url = "jdbc:postgresql://localhost:5432/300387709_jdbc";
     private Connection conManager;
-    private Statement stmt;
     private String errorMessage = "\nThere are no matching record according to the information you supplied." +
                             "\nPlease check your input and try again";
     private String p16Spaces = "                ";
+    private String divider = "\n==========================================================\n";
+    private Statement stmt;
+    private ResultSet rs;
 
 
 
     public LibraryModel(JFrame parent, String userid, String password) {
 	dialogParent = parent;
 	try {
-	    //set up the connections
+	    //set up the connections,initialize the statement object
         conManager = DriverManager.getConnection(url,userid,password);
-
+        stmt = conManager.createStatement();
     }catch (java.sql.SQLException e) {e.printStackTrace();}
     }
 
     public String bookLookup(int isbn) {
 
-        ResultSet rs;
         StringBuilder qResult =  new StringBuilder();
         String banner = "Please check the  query result of book look up is below:\n";
         qResult.append(banner);
         try {
-            stmt  = conManager.createStatement();
             rs = stmt.executeQuery("SELECT * FROM book WHERE isbn= "+isbn+";");
             //Get the info from books first
             if (rs.next()) {
-                qResult.append("\n  ISBN:  ").append(rs.getInt(1));
-                qResult.append("\n  Title:  ") .append(rs.getString(2));
-                qResult.append("\n  Edition No. :  ").append(rs.getString(3));
-                qResult.append("\n  Numbers of copies:  ").append(rs.getString(4));
-                qResult.append("\n  Number of copies left:  ").append(rs.getString(5));
-                qResult.append("\n  Authors: ");
-                //Then retrive the
+                qResult.append("\nISBN: ").append(rs.getInt(1));
+                qResult.append("\nTitle: ") .append(rs.getString(2));
+                qResult.append("\nEdition NUmber: ").append(rs.getString(3));
+                qResult.append("\nNumbers of copies: ").append(rs.getString(4));
+                qResult.append("\nNumber of copies left: ").append(rs.getString(5));
+                qResult.append("\nAuthors: ");
+                //Then retrieve the Author's infomation
                 rs = stmt.executeQuery("SELECT name,surname,authorseqno" +
                                          " FROM book_author NATURAL JOIN author" +
                                          " WHERE isbn =" +isbn+
                                          " ORDER BY authorseqno ASC;");
-
                 while (rs.next()){
-                    qResult.append(rs.getString("name"));
-                    qResult.append(rs.getString("surname"));
-                    qResult.append("Author Seq No.: ").append(rs.getString("authorseqno")).append("\n");;
+                    qResult.append(rs.getString(1));
+                    qResult.append(rs.getString(2));
+                    qResult.append("Author Seq No.: ").append(rs.getString(3)).append("\n");;
                     qResult.append(p16Spaces);
-
                 }
             }
             else{
@@ -72,27 +70,132 @@ public class LibraryModel {
     }
 
     public String showCatalogue() {
-	return "Show Catalogue Stub";
+
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check the information of all the books (Catalogue) below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT * FROM book ;");
+            if(!rs.next()) {
+                qResult.append(errorMessage);
+                return qResult.toString();
+            }
+            while(rs.next()) {
+                qResult.append(divider);
+                qResult.append("\nISBN: ").append(rs.getInt(1));
+                qResult.append("\nTitle: ") .append(rs.getString(2));
+                qResult.append("\nEdition Number: ").append(rs.getString(3));
+                qResult.append("\nNumbers of copies: ").append(rs.getString(4));
+                qResult.append("\nNumber of copies left: ").append(rs.getString(5));
+            }
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String showLoanedBooks() {
-	return "Show Loaned Books Stub";
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check the information of loaned books below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT isbn,title,customerid,f_name,l_name,duedate"+
+                                        " FROM (book natural join cust_book) natural join customer;");
+            if(!rs.next()) {
+                qResult.append(errorMessage);
+                return qResult.toString();
+            }
+            while(rs.next()) {
+                qResult.append("\nISBN: ").append(rs.getInt(1));
+                qResult.append("\nTitle: ").append(rs.getString(2));
+                qResult.append("\nCustomer  ID : ").append(rs.getString(3));
+                qResult.append("\nCustomer Name: ").append(rs.getString(4))
+                                                     .append("   ").append(rs.getString(5));
+                qResult.append("\nDue Date: ").append(rs.getString(6));
+            }
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String showAuthor(int authorID) {
-	return "Show Author Stub";
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check the Author's information of your query below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT * FROM author WHERE authorid ="+authorID+";");
+
+            if(rs.next()) {
+                qResult.append("\nAuthor  ID: ").append(rs.getInt(1));
+                qResult.append("\nFirst Name: ").append(rs.getString(2));
+                qResult.append("\nLast  Name: ").append(rs.getString(3));
+            }
+            else{
+                qResult.append(errorMessage);
+            }
+
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String showAllAuthors() {
-	return "Show All Authors Stub";
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check all the Author's information below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT * FROM author ;");
+            if(!rs.next()) {
+                qResult.append(errorMessage);
+                return qResult.toString();
+            }
+            while(rs.next()) {
+                qResult.append(divider);
+                qResult.append("\nAuthor  ID: ").append(rs.getInt(1));
+                qResult.append("\nFirst Name: ").append(rs.getString(2));
+                qResult.append("\nLast  Name: ").append(rs.getString(3));
+            }
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String showCustomer(int customerID) {
-	return "Show Customer Stub";
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check the Customers' information of your query below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT * FROM customer WHERE customerid ="+customerID+";");
+
+            if(rs.next()) {
+                qResult.append("\nCustomer ID:  ").append(rs.getInt(1));
+                qResult.append("\nFirst  Name:  ") .append(rs.getString(2));
+                qResult.append("\nLast   Name:  ").append(rs.getString(3));
+                qResult.append("\ncity:  ").append(rs.getString(4));
+            }
+            else{
+                qResult.append(errorMessage);
+            }
+
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String showAllCustomers() {
-	return "Show All Customers Stub";
+        StringBuilder qResult =  new StringBuilder();
+        String banner = "Please check all the Customers' information below:\n";
+        qResult.append(banner);
+        try {
+            rs = stmt.executeQuery("SELECT * FROM customer ;");
+
+            if(!rs.next()) {
+                qResult.append(errorMessage);
+                return qResult.toString();
+            }
+            while(rs.next()) {
+                qResult.append(divider);
+                qResult.append("\nCustomer ID:  ").append(rs.getInt(1));
+                qResult.append("\nFirst  Name:  ") .append(rs.getString(2));
+                qResult.append("\nLast   Name:  ").append(rs.getString(3));
+                qResult.append("\ncity:  ").append(rs.getString(4));
+            }
+        }catch (SQLException e) {e.printStackTrace();}
+        return qResult.toString();
     }
 
     public String borrowBook(int isbn, int customerID,
@@ -105,6 +208,11 @@ public class LibraryModel {
     }
 
     public void closeDBConnection() {
+        try{
+            rs.close();
+            stmt.close();
+            conManager.close();
+        }catch (java.sql.SQLException e){e.printStackTrace();}
     }
     
     public String deleteCus(int customerID) {
